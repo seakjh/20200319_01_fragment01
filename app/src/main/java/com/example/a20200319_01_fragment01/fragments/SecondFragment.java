3b18fc2e45c23b751a83b9888e34ce6dff10c5a6
seakjh <skykjh0513@gmail.com>
2020-03-19 11:19:01 +0900

    1234

--- app/src/main/java/com/example/a20200319_01_fragment01/fragments/SecondFragment.java
+++ app/src/main/java/com/example/a20200319_01_fragment01/fragments/SecondFragment.java
@@ -7,23 +7,33 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 
 import com.example.a20200319_01_fragment01.R;
+import com.example.a20200319_01_fragment01.databinding.FragmentSecondBinding;
 
 public class SecondFragment extends Fragment {
+
+    FragmentSecondBinding binding = null;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_second, container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
+        binding.loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
+            }
+        });
 
     }
 }
